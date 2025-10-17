67c3e4b80bc4950c6c3e1025d27dfbcc3927c8b9
Irsan <muhdirsan181@gmail.com>
2025-10-17 09:33:06 +0800

    completed the tips and emergency fragment. Neext the admin dashboard to add, edit, and delete

--- app/src/main/java/com/example/alertmate/TipsFragment.kt
+++ app/src/main/java/com/example/alertmate/TipsFragment.kt
@@ -22,7 +22,7 @@ class TipsFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_tips, container, false)
     }
-
+//
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
